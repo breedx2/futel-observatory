89e3771c19607f4e896ec9a77aefc5d515ba933d
jason <jason@noisybox.net>
2020-11-18 17:15:28 -0800

    css is hard

--- src/components/Caller.js
+++ src/components/Caller.js
@@ -9,21 +9,9 @@ class Caller extends Component {
 
   render() {
     return (
-      <div clasName="col m2">
-        <div className="row caller-container">
-          <div className="col s12 m6">
-            <div className="card brown lighten-2">
-              <div className="card-content white-text">
-                <span className="card-title text-bold"><h3>Some Caller</h3></span>
-                <p>text + visual description of what they're doing</p>
-              </div>
-              <div className="card-action">
-                <a href="#">other?</a>
-                <a href="#">info?</a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="caller">
+      <h3>caller</h3>
+      they are doing something
       </div>
     )
   }
--- src/components/CallersRow.js
+++ src/components/CallersRow.js
@@ -6,22 +6,10 @@ class CallersRow extends Component {
 
   render() {
     return (
-      <div>
-          <div className="row">
-            <div className="col s1">test1</div>
-            <div className="col s1">test2</div>
-            <div className="col s1">test3</div>
-            <div className="col s1">test4</div>
-            <div className="col s1">test5</div>
-            <div className="col s1">test6</div>
-            <div className="col s1">test7</div>
-            <div className="col s1">test8</div>
-            <div className="col s1">test9</div>
-            <div className="col s1">test10</div>
-            <div className="col s1">test11</div>
-            <div className="col s1">test12</div>
-          </div>
-        <div className="row">
+      <div className="full-height valign-wrapper center-align">
+        <div className="valign-wrapper center-align allCallers ">
+          <Caller/>
+          <Caller/>
           <Caller/>
           <Caller/>
         </div>
